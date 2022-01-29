76d3d6ca7ce50a997871a3c4e238f9c326c29970
Scott Quach <scottqglobal@gmail.com>
2022-01-29 14:06:01 -0800

    Hot reload "npm start" script working

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,16 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info('Hello logs!', { structuredData: true });
+    response.send('Hello from Firebase!');
+});
+
+export const getWeather = functions.https.onRequest(async (request, response) => {
+    // const res = await fetch('https://api.openweathermap.org/data/2.5/weather', {});
+    response.send('Start script test');
+});
