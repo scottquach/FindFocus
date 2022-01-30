fdbe99dc16c2b7a861dcd45701763188cb4f693d
Scott <scottqglobal@gmail.com>
2022-01-29 21:52:04 -0800

    Added window and pet rooms

--- src/models/rooms/window.model.ts
+++ src/models/rooms/window.model.ts
@@ -1,1 +1,39 @@
-export const WindowRooms = [];
+import { CategoryId } from '../category.model';
+
+export const WindowRooms = [
+    {
+        id: 'DRbRIXMmHtY',
+        name: 'Thunderstorm in New york',
+        creator: 'Nomadic Ambience',
+        link: 'https://www.youtube.com/watch?v=DRbRIXMmHtY&t=857s&ab_channel=NomadicAmbience',
+        category: CategoryId.Window,
+    },
+    {
+        id: 'kAM7wnaF6KM',
+        name: 'Downtown Los Angeles',
+        creator: 'OpticStream360',
+        link: 'https://www.youtube.com/watch?v=kAM7wnaF6KM&ab_channel=OpticStream360',
+        category: CategoryId.Window,
+    },
+    {
+        id: '7V_2dAcCgQU',
+        name: 'Mountain View',
+        creator: 'OpticStream360',
+        link: 'https://www.youtube.com/watch?v=7V_2dAcCgQU&ab_channel=OpticStream360',
+        category: CategoryId.Window,
+    },
+    {
+        id: 'Vg1mpD1BICI',
+        name: 'Open Window New York City',
+        creator: 'Nomadic Ambience',
+        link: 'https://www.youtube.com/watch?v=Vg1mpD1BICI&t=5775s&ab_channel=NomadicAmbience',
+        category: CategoryId.Window,
+    },
+    {
+        id: 'WdFmdsTdxjY',
+        name: 'Relaxing rain window view',
+        creator: 'OpticStream360',
+        link: 'https://www.youtube.com/watch?v=WdFmdsTdxjY&ab_channel=OpticStream360',
+        category: CategoryId.Window,
+    },
+];
