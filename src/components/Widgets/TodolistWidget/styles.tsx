bcd2ca6b7c26e64686e2702822e44fe80d2be797
Scott Quach <scottqglobal@gmail.com>
2022-10-03 20:45:36 -0700

    Fixed styling with todo list

--- src/components/Widgets/TodolistWidget/styles.tsx
+++ src/components/Widgets/TodolistWidget/styles.tsx
@@ -6,21 +6,23 @@ export const Content = styled.div`
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	justify-content: center;
+	justify-content: flex-start;
 	width: 100%;
 	height: 100%;
 	min-width: 5rem;
 	min-height: 5rem;
 `
 
-export const Note = styled.textarea`
+export const Note = styled.input<{ done: boolean }>`
 	resize: none;
 	border: none;
 	outline: none;
 	width: 100%;
 	background-color: var(--color-background);
 	color: var(--color-on-background);
-	margin-top: 1.5rem;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	text-decoration: ${props => props.done ? 'line-through' : ''};
 `
 
 export const TaskInstance = styled.div`
@@ -32,6 +34,11 @@ export const TaskInstance = styled.div`
 `
 
 export const Button = styled.button`
-	background-color: var(--color-background);
-	color: var(--color-on-background);
+	/* background-color: var(--color-primary-light); */
+	color: var(--color-primary-light);
+	margin-top: .5rem;
+	margin-bottom: .5rem;
+	/* border: solid 1px var(--color-primary); */
+	border-radius: 8px;
+	padding: 0 .5rem;
 `
